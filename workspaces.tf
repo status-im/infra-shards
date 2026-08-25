dcaa56bf87370602665cc3c294924a325b2cc117
Vedran Mendelski <vedran@status.im>
2026-08-25 15:09:22 +0200

    status.prod: downscale ac store-db
    Ref.: https://github.com/status-im/infra-status/issues/68

--- workspaces.tf
+++ workspaces.tf
@@ -49,7 +49,7 @@ locals {
       store_gc_type = "c2d-standard-4"
 
       store_db_do_type = "c2-16vcpu-32gb-intel"
-      store_db_ac_type = "ecs.c6.4xlarge"
+      store_db_ac_type = "ecs.r6.large"
       store_db_gc_type = "c2d-highmem-2"
 
       store_db_data_vol_size = 320
