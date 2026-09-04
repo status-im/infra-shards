74689dedd7cc7da6237d299c4a3666ec6491f1a0
Vedran Mendelski <vedran@status.im>
2026-09-04 15:14:14 +0200

    status.prod: downscale DO store-db
    Ref.: https://github.com/status-im/infra-status/issues/68

--- workspaces.tf
+++ workspaces.tf
@@ -48,7 +48,7 @@ locals {
       store_ac_type = "ecs.t5-lc1m4.large"
       store_gc_type = "c2d-standard-4"
 
-      store_db_do_type = "c2-16vcpu-32gb-intel"
+      store_db_do_type = "m-2vcpu-16gb"
       store_db_ac_type = "ecs.r6.large"
       store_db_gc_type = "c2d-highmem-2"
 
